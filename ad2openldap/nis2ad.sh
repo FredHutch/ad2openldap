fcba763c65891097e9108d1be483bec683acda21
Dirk Petersen <dirkpetersen@users.noreply.github.com>
2019-10-26 20:25:38 -0700

    Update nis2ad.sh
    a few more conditions and checks

--- ad2openldap/nis2ad.sh
+++ ad2openldap/nis2ad.sh
@@ -5,6 +5,11 @@
 ## users, groups or other NIS information
 ## it is intended for one time migration 
 ## from NIS to LDAP 
+## Tried to get these tools to work
+## www.padl.com/download/MigrationTools.tgz
+## but could not.
+## current version of this script is in 
+## https://github.com/FredHutch/ad2openldap/
 
 mydir="${0%/*}"
 . ${mydir}/nis2ad.cfg
@@ -22,11 +27,20 @@ echo "" >> $ldif_map
 echo "dn: CN=auto.master,${OU}" >> $ldif_map
 echo -e "objectClass: top\nobjectClass: nisMap" >> $ldif_map
 echo -e "nisMapName: auto.master\n" >> $ldif_map
-
-tail --lines=+3 /etc/auto.master | while read line;  do
+if [[ -f /etc/auto.master ]]; then
+  amcmd='tail --lines=+2 /etc/auto.master' # start import at second line
+else
+  amcmd="ypcat -k auto.master"
+fi
+$amcmd | while read line; do
   mount=${line%% *}
-  rest=${line#*:}
+  rest=${line#* }
+  rest=${rest#"yp:"}
   map=${rest%% *}
+  if [[ -f $map ]]; then
+    echo "${map}: local maps are not supported, only NIS maps (except /etc/auto.master)"
+    continue
+  fi
   echo "dn: CN=${map},${OU}" >> $ldif_map
   echo -e "objectClass: top\nobjectClass: nisMap" >> $ldif_map
   echo "nisMapName: ${map}" >> $ldif_map
@@ -73,7 +87,7 @@ done
 echo "$ldif_grp"
 
 echo "${LDAPADD} -x -h ${LDAPHOST} -D \"${LDAP_BINDDN}\" -w \"XXXXXXXX\" -f ${ldif_map} -f ${ldif_grp}"
-read -p "Do you now want to import these ldif files to AD using this command line? " -n 1 -r -t 60
+read -p "Do you want to import the ldif files into AD using the above command line now? " -n 1 -r -t 60
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   ${LDAPADD} -x -h ${LDAPHOST} -D "${LDAP_BINDDN}" -w "${LDAP_BINDCRED}" -f ${ldif_map} -f ${ldif_grp}
 fi
